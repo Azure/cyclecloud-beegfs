3bb1eb08a4df5718f7f4c3858018a88f4a3e2f32
Jer-Ming Chia <jer-ming.chia@cyclecomputing.com>
2018-08-10 16:37:23 +0800

    If kernel-devel install fails attempt to get it from Centos Vault

--- specs/default/chef/site-cookbooks/beegfs/recipes/client.rb
+++ specs/default/chef/site-cookbooks/beegfs/recipes/client.rb
@@ -1,13 +1,22 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
 include_recipe "::default"
 %w{beegfs-client beegfs-helperd beegfs-utils gcc gcc-c++}.each { |p| package p }
 
 # Problem with some images running an outdated kernel version,
 # where the kernel headers don't exist in the repos anymore.
 # mitigate some failures by checking if kernel-devel has already been installed
-execute "Installing kernel devel version that matches kernel" do
+
+# Try specifing the kernel version and doing a yum install.
+# ignore failues in this first try
+execute "Installing kernel-devel version that matches kernel" do
   command 'yum install -y "kernel-devel-uname-r == $(uname -r)"'
+  not_if "rpm -qa | grep kernel-devel | grep $(uname -r)"
+  ignore_failure true
+end
+
+# If the above fails, update the centos-release, enable all repos
+# and try installing kernel-devel again
+execute "Enabling Centos vault repos and installing kernel-devel version that matches kernel" do
+  command 'yum install -y centos-release; yum install -y --enablerepo=C*-base --enablerepo=C*-updates kernel-devel-$(uname -r)'
   not_if "rpm -qa | grep kernel-devel | grep $(uname -r)"
 end
 
