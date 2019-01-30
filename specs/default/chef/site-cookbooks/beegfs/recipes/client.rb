82a56ab58dfcaf2c223dc6bf484d76156aa6c1a6
Eric G. Wolfe <eric.wolfe@microsoft.com>
2019-01-30 17:09:31 -0500

    Fix client recipe for Ubuntu

--- specs/default/chef/site-cookbooks/beegfs/recipes/client.rb
+++ specs/default/chef/site-cookbooks/beegfs/recipes/client.rb
@@ -1,5 +1,13 @@
 include_recipe "::default"
-%w{beegfs-client beegfs-helperd beegfs-utils gcc gcc-c++}.each { |p| package p }
+
+packages = case node['platform_family']
+           when 'rhel'
+             %w{beegfs-client beegfs-helperd beegfs-utils gcc gcc-c++}
+           when 'debian'
+             %w{beegfs-client beegfs-helperd beegfs-utils gcc cpp}
+           end
+
+packages.each { |p| package p }
 
 # Problem with some images running an outdated kernel version,
 # where the kernel headers don't exist in the repos anymore.
@@ -12,13 +20,15 @@ include_recipe "::default"
     gpgkey "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-#{node[:platform_version].to_i}"
     action :create
     not_if "yum list kernel-devel-$(uname -r)"
+    only_if { node['platform_family'] == 'rhel' }
   end
-end 
+end
 
 # Install the kernel-devel package for our platform
 execute "Installing kernel-devel version that matches kernel" do
   command 'yum install -y "kernel-devel-uname-r == $(uname -r)"'
   not_if "rpm -qa | grep kernel-devel | grep $(uname -r)"
+  only_if { node['platform_family'] == 'rhel' }
 end
 
 service "beegfs-helperd" do
@@ -28,7 +38,7 @@ end
 execute "Mount BeeGFS" do
   command "echo '#{node["beegfs"]["mount_point"]} /etc/beegfs/beegfs-client.conf' > /etc/beegfs/beegfs-mounts.conf"
   not_if "grep -q '#{node["beegfs"]["mount_point"]}' /etc/beegfs/beegfs-mounts.conf"
-  notifies :restart, 'service[beegfs-client]', :immediately  
+  notifies :restart, 'service[beegfs-client]', :immediately
 end
 
 service "beegfs-client" do
