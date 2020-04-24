481319e876c71764732c88f329c24270c4dd24ae
Andy Howard <anhoward@microsoft.com>
2020-04-24 14:47:20 +0000

    Update Beeond recipe to work with repo changes

--- specs/default/chef/site-cookbooks/beegfs/recipes/beeond.rb
+++ specs/default/chef/site-cookbooks/beegfs/recipes/beeond.rb
@@ -1,13 +1,8 @@
-beegfs_repo_file = "/etc/yum.repos.d/beegfs.repo"
-beegfs_rpm_gpg_key = node['beegfs']['rpm_gpg_key']
-
-remote_file "BeeGFS repo file" do
-  path beegfs_repo_file
-  source node['beegfs']['repo_file_url']
-end
-
-execute "Import rpm gpg key" do
-  command "rpm --import #{beegfs_rpm_gpg_key}"
+yum_repository 'beegfs' do
+  description "BeeGFS #{node['beegfs']['repo']['version']} (rhel#{node['platform_version'].to_i})"
+  baseurl node['beegfs']['repo']['yum']['baseurl']
+  gpgkey node['beegfs']['repo']['yum']['gpgkey']
+  only_if { node['platform_family'] == 'rhel' }
 end
 
 %w{beegfs-client beegfs-helperd beegfs-utils beeond gcc gcc-c++}.each { |p| package p }
@@ -60,6 +55,16 @@ cookbook_file "/opt/beegfs/sbin/beeond" do
   action :create
   source "beeond"
   mode 0755
+end
+
+# On some centos 7 images running RDMA networking we may need to rebuild the beegfs client to support
+# the networking options - see https://community.mellanox.com/s/article/howto-configure-and-test-beegfs-with-rdma
+bash "Update beegfs-client autobuild.conf" do
+  code <<-'EOH'
+sed -i 's/^buildArgs=.*/buildArgs=-j8 BEEGFS_OPENTK_IBVERBS=1 OFED_INCLUDE_PATH=\/usr\/src\/ofa_kernel\/default\/include\//' /etc/beegfs/beegfs-client-autobuild.conf
+  EOH
+  only_if { node['platform_family'] == 'rhel' }
+  only_if { ::File.directory? "/usr/src/ofa_kernel/default/include/" }
 end
 
 execute "Build BeeGFS module" do
